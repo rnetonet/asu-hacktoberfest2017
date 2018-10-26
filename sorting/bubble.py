26866d064b8fec69b9d64f052968f1293be52081
ruivaldo.lobao <ruivaldo.lobao@ifbaiano.edu.br>
2018-10-26 09:33:40 -0300

    Adjust bubble

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -1,12 +1,11 @@
 def bubble(numbers):
 	n = len(numbers)
+
 	# traverse through all elements in the array
 	for i in range(n):
 		for j in range(0, n-i-1):
+            # swap if bigger
 			if numbers[j] > numbers[j+1]:
-				# TODO: swap numbers if element is greater
-				temp = numbers[j]
-				numbers[j] = numbers[j + 1]
-				numbers[j + 1] = temp
+				numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
 
-	return numbers;
+	return numbers
